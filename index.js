eec93aacfa3044e0ef66f26ac4a4456ed5c18399
SKumar <sk.kumar561@gmail.com>
2022-08-03 02:44:29 +0530

    first commit

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 const hostname = '127.0.0.1';
 const port = 8000;
 
+app.use(express.json());
 app.use('/', route);
 
 app.listen(port, hostname, () => {
--- routes/route.js
+++ routes/route.js
@@ -6,4 +6,10 @@ router.get('/test', function (req, res) {
     res.send("Hello World!");
 });
 
+router.post('/create', function (req, res) {
+    let data = req.body
+    console.log(data);
+    res.send(data);
+});
+
 module.exports = router;
