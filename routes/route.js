eaf0ab8225b03fb470f0a04fed8abaaccb7f86bc
SKumar <sk.kumar561@gmail.com>
2022-08-03 03:03:54 +0530

    first commit

--- routes/route.js
+++ routes/route.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/test', function (req, res) {
-    console.log("Hello World!");
-    res.send("Hello World!");
-});
-
 router.post('/create', function (req, res) {
     let data = req.body
     console.log(data);
     res.send(data);
 });
+
+router.get('/get', function (req, res) {
+    console.log(req.query);
+    res.send(req.query);
+});
+
+router.get('/get/:id', function (req, res) {
+    let id = req.params.id
+    console.log(id);
+    res.send(id);
+});
+
+router.put('/', function (req, res) {
+
+})
+
+router.delete('/delete', function (req, res) {
+    
+})
 
 module.exports = router;
