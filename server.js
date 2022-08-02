21df8f9869e46b3da5e19c027991ca92ca417b0b
SKumar <sk.kumar561@gmail.com>
2022-08-03 01:24:22 +0530

    first commit

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const http = require('http');
+const express = require('express');
+const app = express();
 const hostname = '127.0.0.1';
 const port = 8000
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('Hello World!')
+app.get('/', function (req, res) {
+    console.log("Hello World!");
+    res.send("Hello World!");
 })
 
-server.listen(port, hostname, () => {
+app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}`);
 });
