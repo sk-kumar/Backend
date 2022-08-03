9dac6f0559e151cfc1567510b65c0427afee938e
SKumar <sk.kumar561@gmail.com>
2022-08-03 10:52:58 +0530

    first commit

--- routes/route.js
+++ routes/route.js
@@ -3,14 +3,14 @@ const userModel = require('../models/userModel');
 const router = express.Router();
 
 // POST
-router.post('/user', async function (req, res) {
+router.post('/user', async (req, res)=> {
     let data = await userModel.create(req.body);
     console.log(data);
     res.send(data);
 });
 
 // GET
-router.get('/user/:id', async function (req, res) {
+router.get('/user/:id', async (req, res)=> {
     let id = req.params.id
     let user = await userModel.findById(id)
     console.log(user);
@@ -18,19 +18,19 @@ router.get('/user/:id', async function (req, res) {
 });
 
 // PUT
-router.put('/user/:id', async function (req, res) {
+router.put('/user/:id', async (req, res) => {
     let id = req.params.id
-    let user = await userModel.findByIdAndUpdate(id, { $set: req.body },{new:true});
+    let user = await userModel.findByIdAndUpdate(id, { $set: req.body }, { new: true });
     console.log(user);
     res.send(user);
-})
+});
 
 // DELETE
-router.delete('/user/:id', async  function (req, res) {
+router.delete('/user/:id', async (req, res) => {
     let id = req.params.id
     await userModel.findByIdAndDelete(id)
     console.log("User Deleted Successfull");
     res.send("User Deleted Successfull");
-})
+});
 
 module.exports = router;
