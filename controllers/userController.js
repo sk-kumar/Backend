3540585df7ba38c61e05fa5b0a45a6dfdbe3d06c
SKumar <sk.kumar561@gmail.com>
2022-08-03 11:42:59 +0530

    first commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,26 @@
 const userModel = require('../models/userModel.js');
 
-const createUser = async function (req, res) {
+const createUser = async  (req, res)=> {
     let data = await userModel.create(req.body);
     console.log(data);
     res.send(data);
 }
 
-const getUser = async function (req, res)  {
+const getUser = async  (req, res)=>  {
     let id = req.params.id
     let user = await userModel.findById(id)
     console.log(user);
     res.send(user);
 }
 
-const updateUser = async function (req, res) {
+const updateUser = async  (req, res)=> {
     let id = req.params.id
     let user = await userModel.findByIdAndUpdate(id, { $set: req.body }, { new: true });
     console.log(user);
     res.send(user);
 }
 
-const deleteUser = async function (req, res) {
+const deleteUser = async  (req, res)=> {
     let id = req.params.id
     await userModel.findByIdAndDelete(id)
     console.log("User Deleted Successfull");
